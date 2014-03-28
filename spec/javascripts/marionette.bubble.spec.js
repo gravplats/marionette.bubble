ea3d306d86669f73c456d4e0c583223ad635e595
Mattias Rydengren <mattias.rydengren@coderesque.com>
2014-03-28 23:17:16 +0100

    A layout that bubbles events from the current regions.

--- spec/javascripts/marionette.bubble.spec.js
+++ spec/javascripts/marionette.bubble.spec.js
@@ -1,4 +1,4 @@
-describe('bubble', function() {
+describe('Marionette.BubbleRegion', function() {
   'use strict';
 
   var ItemView;
@@ -32,7 +32,7 @@ describe('bubble', function() {
 
     it('should be bubbled by region', function() {
       expect(bubbled).toBeTruthy();
-    })
+    });
   });
 
   describe('when region has closed view', function() {
@@ -54,6 +54,63 @@ describe('bubble', function() {
 
     it('should no longer bubble events from view', function() {
       expect(bubbled).toBeFalsy();
+    });
+  });
+
+});
+
+describe('Marionette.BubbleLayout', function() {
+
+  var Layout, ItemView;
+
+  beforeEach(function () {
+    loadFixtures('itemview-template.html');
+
+    Layout = Marionette.BubbleLayout.extend({
+      template: '#layout-template',
+      regions: {
+        'content': '#region'
+      }
+    });
+
+    ItemView = Marionette.ItemView.extend({
+      template: '#itemview-template',
+
+      raise: function() {
+        this.trigger('custom:itemview:event');
+      }
+    });
+  });
+
+  describe('when adding a region to layout', function() {
+    var layout;
+
+    beforeEach(function() {
+      layout = new Layout({ el: '#layout' });
+    });
+
+    it('should be a Marionette.BubbleRegion', function() {
+      expect(layout.content instanceof Marionette.BubbleRegion).toBeTruthy();
+    });
+  });
+
+  describe('when view is triggering an event', function() {
+    var layout, bubbled;
+
+    beforeEach(function() {
+      layout = new Layout({ el: '#layout' });
+      layout.on('custom:itemview:event', function() {
+        bubbled = true;
+      });
+
+      var view = new ItemView();
+      layout.content.show(view);
+
+      view.raise();
+    });
+
+    it('should be bubbled by layout', function() {
+      expect(bubbled).toBeTruthy();
     });
   });
 
--- src/marionette.bubble.js
+++ src/marionette.bubble.js
@@ -3,8 +3,14 @@ Marionette.BubbleRegion = (function(Marionette, Backbone, _) {
 
   return Marionette.Region.extend({
 
+    ignoredEventNames: [ 'before:close', 'before:show', 'close', 'render', 'show '],
+
     onBeforeShow: function(view) {
-      this.listenTo(view, 'all', function() {
+      this.listenTo(view, 'all', function(eventName) {
+        if (_(this.ignoredEventNames).contains(eventName)) {
+          return;
+        }
+
         this.trigger.apply(this, arguments);
       });
     },
@@ -15,5 +21,35 @@ Marionette.BubbleRegion = (function(Marionette, Backbone, _) {
 
   });
 
+})(Marionette, Backbone, _);
+
+Marionette.BubbleLayout = (function(Marionette, Backbone, _) {
+  'use strict';
+
+  return Marionette.Layout.extend({
+
+    ignoredEventNames: [ 'before:show', 'show '],
+
+    constructor: function() {
+      this.listenTo(this, 'region:add', function(name, region) {
+        this.listenTo(region, 'all', function(eventName) {
+          if (_(this.ignoredEventNames).contains(eventName)) {
+            return;
+          }
+
+          this.trigger.apply(this, arguments);
+        });
+      });
+
+      this.listenTo(this, 'region:remove', function(name, region) {
+        this.stopListening(region, 'all');
+      });
+
+      Marionette.Layout.prototype.constructor.apply(this, arguments);
+    },
+
+    regionType: Marionette.BubbleRegion
+
+  });
 
 })(Marionette, Backbone, _);
